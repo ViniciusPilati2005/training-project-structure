7fd26b7996bfe29c92f33ddaf17aa15c6fca5c53
Vinicius Raitz Pilati <vini1718a@gmail.com>
2025-07-26 21:11:22 -0300

    feat(login-view.tsx): add login form component

---
+++ src/pages/login/components/login-form.tsx
@@ -0,0 +1,54 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+export function LoginForm() {
+  return (
+    <Card className="w-full max-w-sm">
+      <CardHeader>
+        <CardTitle>Faça login na sua conta</CardTitle>
+        <CardDescription>
+          Insira o email e a senha para continuar
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div className="flex flex-col gap-6">
+            <div className="grid gap-3">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="m@example.com"
+                required
+              />
+            </div>
+            <div className="grid gap-3">
+              <div className="flex items-center">
+                <Label htmlFor="password">Senha</Label>
+                <a
+                  href="#"
+                  className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                >
+                  Esqueceu sua senha?
+                </a>
+              </div>
+              <Input id="password" type="password" required />
+            </div>
+
+            <Button type="submit" className="w-full">
+              Continuar
+            </Button>
+          </div>
+        </form>
+      </CardContent>
+    </Card>
+  );
+}
--- src/pages/login/login-view.tsx
+++ src/pages/login/login-view.tsx
@@ -1,10 +1,9 @@
-import { Button } from "@/components/ui/button";
+import { LoginForm } from "./components/login-form";
 
 export function LoginView() {
   return (
-    <div>
-      <h1>Login</h1>
-      <Button>Click me</Button>
+    <div className="flex flex-col flex-1 items-center justify-center h-screen">
+      <LoginForm />
     </div>
   );
 }
