6de2811706d2ca504d1f8d4b93ed5304ff75db8d
Vinicius Raitz Pilati <vini1718a@gmail.com>
2025-07-26 21:02:38 -0300

    chore(package.json): install login form dependencies and configure shad cn for components

--- src/pages/login/login-view.tsx
+++ src/pages/login/login-view.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export function LoginView() {
   return (
     <div>
       <h1>Login</h1>
+      <Button>Click me</Button>
     </div>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: { open: true },
   plugins: [
     tanstackRouter({
       target: "react",
@@ -13,4 +15,9 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
